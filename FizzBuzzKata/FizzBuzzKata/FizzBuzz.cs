d28ec5361be9dc97a68652e2f873f641b251e1e3
James Hollingshead <git@jameshollingshead.com>
2017-07-31 12:26:12 -0400

    FizzBuzz completed

--- FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -8,6 +8,32 @@ namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
-        
+
+        public string GetFizzBuzz(int number)
+        {
+            if (IsEvenlyDivisbleByThree(number) && IsEvenlyDivisibleByFive(number))
+                return "FizzBuzz";
+
+            if (IsEvenlyDivisbleByThree(number))
+                return "Fizz";
+
+            if (IsEvenlyDivisibleByFive(number))
+                return "Buzz";
+
+            return number.ToString();
+        }
+
+
+        public bool IsEvenlyDivisbleByThree(int number)
+        {
+            return number % 3 == 0;
+        }
+
+        public bool IsEvenlyDivisibleByFive(int number)
+        {
+            return number % 5 == 0;
+        }
+
+
     }
 }
--- FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTest.cs
+++ FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTest.cs
@@ -10,8 +10,11 @@ namespace FizzBuzzKataTests
 {
     [TestFixture]
     public class FizzBuzzTest
-    {
+    {   
         private FizzBuzz fizzBuzz;
+
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
 
         [SetUp]
         public void SetUp()
@@ -19,6 +22,53 @@ namespace FizzBuzzKataTests
             fizzBuzz = new FizzBuzz();
         }
 
-        
+        [Test]
+        public void InputOfOneReturns1()
+        {
+            var result = fizzBuzz.GetFizzBuzz(1);
+            Assert.AreEqual("1", result);
+        }
+
+        [Test]
+        public void InputOfTwoReturns2()
+        {
+            var result = fizzBuzz.GetFizzBuzz(2);
+            Assert.AreEqual("2", result);
+        }
+
+        [Test]
+        public void InputOfThreeReturnsFizz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(3);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void InputOfFiveReturnsBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(5);
+            Assert.AreEqual("Buzz",result);
+        }
+
+        [Test]
+        public void InputOfSixReturnsFizz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(6);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void InputOfTenReturnsBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(10);
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [Test]
+        public void InputOfFifteenReturnsFizzBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(15);
+            Assert.AreEqual("FizzBuzz", result);
+        }
     }
 }
